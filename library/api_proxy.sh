72da1f7f1ada2cf5fa8683ee1f6b50234c72415b
Melvin <31448155+melvio@users.noreply.github.com>
2021-07-29 20:12:13 +0200

    created a working post_curl

--- library/api_proxy.sh
+++ library/api_proxy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 readonly x_client_header="79551d98-31e9-42b4-b7fa-9d89b0944319-hopla"
-domain="https://habitica.com"
+readonly domain="https://habitica.com"
 export api_version="v3"
 api_base_url="${domain}/api/${api_version}"
+
 
 get_curl() {
   url_path="$1"
@@ -19,5 +20,13 @@ get_curl() {
 post_curl() {
   url_path="$1"
   json_data="$2"
+
+  curl --silent -XPOST "${api_base_url}/${url_path}" \
+    --header "Content-Type: application/json" \
+    --header "x-api-user: ${user_id}" \
+    --header "x-api-key: ${api_token}" \
+    --header "x-client ${x_client_header}" \
+    --data "${json_data}"
+
 
 }
