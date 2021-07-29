8305c5a3b55115df4bc308d6a10e86a33bd4dd2d
Melvin <31448155+melvio@users.noreply.github.com>
2021-07-29 21:10:34 +0200

    implemented exponential backoff to prevent overloading the habitica api

--- library/api_proxy.sh
+++ library/api_proxy.sh
@@ -6,25 +6,36 @@ export api_version="v3"
 api_base_url="${domain}/api/${api_version}"
 
 
+
+# Prevent overloading the habitica API by using an exponential backoff algorithm.
+# Specifically, retry on failure at respectively 1, 2, 4, 8, 16, 32, 64, 128, and 256 seconds
+readonly curl_retry=9
+readonly curl_retry_max_time=260
+
 get_curl() {
   url_path="$1"
 
-  curl --silent "${api_base_url}/${url_path}" --compressed \
+  http_response=$(curl --silent --show-error "${api_base_url}/${url_path}" --compressed \
+    --retry "${curl_retry}" \
+    --retry-max-time "${curl_retry_max_time}" \
     --header "Content-Type: application/json" \
     --header "x-api-user: ${user_id}" \
     --header "x-api-key: ${api_token}" \
-    --header "x-client: ${x_client_header}"
+    --header "x-client: ${x_client_header}")
+  echo "${http_response}"
 }
-
 
 post_curl() {
   url_path="$1"
   json_data="$2"
 
-  curl --silent -XPOST "${api_base_url}/${url_path}" \
+  http_response=$(curl --silent --show-error -XPOST "${api_base_url}/${url_path}" \
+    --retry "${curl_retry}" \
+    --retry-max-time "${curl_retry_max_time}" \
     --header "Content-Type: application/json" \
     --header "x-api-user: ${user_id}" \
     --header "x-api-key: ${api_token}" \
     --header "x-client: ${x_client_header}" \
-    --data "${json_data}"
+    --data "${json_data}")
+  echo "${http_response}"
 }
