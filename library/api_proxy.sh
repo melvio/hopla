933e683d647025f7c96f86e0cd7aaf37d8716f56
Melvin <31448155+melvio@users.noreply.github.com>
2021-07-29 19:57:25 +0200

    implemented get_curl for "hopla api status"

--- library/api_proxy.sh
+++ library/api_proxy.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
+readonly x_client_header="79551d98-31e9-42b4-b7fa-9d89b0944319-hopla"
+domain="https://habitica.com"
+export api_version="v3"
+api_base_url="${domain}/api/${api_version}"
+
+get_curl() {
+  url_path="$1"
+
+  curl --silent "${api_base_url}/${url_path}" --compressed \
+    --header "Content-Type: application/json" \
+    --header "x-api-user: ${user_id}" \
+    --header "x-api-key: ${api_token}" \
+    --header "x-client ${x_client_header}"
+}
 
 
+post_curl() {
+  url_path="$1"
+  json_data="$2"
+
+}
