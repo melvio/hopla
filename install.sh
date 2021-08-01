1fa2a8e3ca8ecad379c5acb3142d300cc27f1083
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-01 09:41:17 +0200

    provide support for installing dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+
+
+install_dependencies() {
+  if ! jq --version >> /dev/null ; then
+    sudo apt install jq
+  fi
+  if ! curl --version >> /dev/null ; then
+    sudo apt install curl
+  fi
+}
+install_dependencies
+
+
 cd -- "$(dirname "$0")" || exit 1
 entry_command="$(realpath ./hopla/hopla)"
 target_dir="/usr/local/bin/"
@@ -7,7 +22,7 @@ target_dir="/usr/local/bin/"
 
 if [[ -f "${entry_command}" && -d "${target_dir}" ]] ; then
   if [[ -h "${target_dir}/hopla" ]] ; then
-    echo "symbolic link already exists in ${target_dir}"
+    echo "symbolic link to hopla already exists in ${target_dir}"
   else
     sudo ln -s "${entry_command}" "${target_dir}"
   fi
