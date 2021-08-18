6690e015b2139a7ab5fd63433ad68465b0f4cff5
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-18 19:45:39 +0200

    Refactor: add classes and increase abstraction: no functional changes

--- set/day-start.py
+++ set/day-start.py
@@ -14,18 +14,38 @@ base_url = f"{domain}/api/{api_version}/user"
 custom_day_start_url = f"{base_url}/custom-day-start"
 
 
-class HoplaAuthorization:
+class AuthorizationConstants:
+    CONFIG_SECTION_CREDENTIALS = "credentials"
+    CONFIG_KEY_USER_ID = "user_id"
+    CONFIG_KEY_API_TOKEN = "api_token"
+
+    GLOBAL_ENV_VAR_XDG_CONFIG_HOME = "XDG_CONFIG_HOME"
+    GLOBAL_ENV_VAR_HOPLA_AUTH_FILE = "HOPLA_AUTH_FILE"
+
+
+class HoplaAuthorizationParser:
+
+    def __init__(self):
+        self.config_parser = ConfigParser()
+
+    def initialize(self):
+        if self.auth_file_exists():
+            self.config_parser.read(self.auth_file)
+        else:
+            # TODO: have better user information (stack traces are not nice!)
+            raise FileNotFoundError(f"could not find authorization file {self.auth_file}"
+                                    f"Try running: `hopla set-hopla credentials")
 
     def auth_file_exists(self):
         return self.auth_file.exists \
                and self.auth_file.is_file()
 
     @property
-    def auth_file(self):
+    def auth_file(self) -> Path:
         """ Get the file with authorization"""
         auth_file: Path
-        hopla_auth_file_env_var = os.environ.get("HOPLA_AUTH_FILE")
-        xdg_config_home_env_var = os.environ.get("XDG_CONFIG_HOME")
+        hopla_auth_file_env_var = os.environ.get(AuthorizationConstants.GLOBAL_ENV_VAR_HOPLA_AUTH_FILE)
+        xdg_config_home_env_var = os.environ.get(AuthorizationConstants.GLOBAL_ENV_VAR_XDG_CONFIG_HOME)
 
         if hopla_auth_file_env_var:
             auth_file = Path(hopla_auth_file_env_var)
@@ -36,27 +56,27 @@ class HoplaAuthorization:
 
         return auth_file.resolve()
 
+    @property
+    def user_id(self):
+        return self.config_parser[AuthorizationConstants.CONFIG_SECTION_CREDENTIALS] \
+            .get(AuthorizationConstants.CONFIG_KEY_USER_ID)
 
-config = ConfigParser()
+    @property
+    def api_token(self):
+        return self.config_parser[AuthorizationConstants.CONFIG_SECTION_CREDENTIALS] \
+            .get(AuthorizationConstants.CONFIG_KEY_API_TOKEN)
 
-AUTH_FILE: Path = HoplaAuthorization().auth_file
-print(f"exists={HoplaAuthorization().auth_file_exists()}")
-config.read(AUTH_FILE)
 
-print(config)
-print(config.sections())
-print(config["credentials"])
+hopla_auth_parser = HoplaAuthorizationParser()
+hopla_auth_parser.initialize()
 
-credentials = config["credentials"]
-user_id = credentials.get("user_id")
-api_token = credentials.get("api_token")
 x_client_id = "79551d98-31e9-42b4-b7fa-9d89b0944319-hopla"
 
 headers = {
     "Content-Type": "application/json",
     "x-client": x_client_id,
-    "x-api-user": user_id,
-    "x-api-key": api_token
+    "x-api-user": hopla_auth_parser.user_id,
+    "x-api-key": hopla_auth_parser.api_token
 }
 
 arg_parser = ArgumentParser()
