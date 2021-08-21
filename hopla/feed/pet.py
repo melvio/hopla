8f3ebc171733b998654d403b5a6ac71ce99247d9
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-21 09:52:39 +0200

    Tinkering: removed hopla.sh and see if we can get stuff back

--- hopla/feed/pet.py
+++ hopla/feed/pet.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 sys.path.append(os.environ.get("script_dirname"))
-import  logging
+import logging
 
 # TODO: temporary, while hopla is in beta
 try:
--- hopla_python_migration.py
+++ hopla_python_migration.py
@@ -5,6 +5,7 @@ import subprocess
 import os
 import logging
 from pathlib import Path
+from argparse import ArgumentParser
 
 # TODO: temporary, while hopla is in beta
 try:
@@ -29,40 +30,107 @@ def setup_logging() -> logging.Logger:
 log = setup_logging()
 
 
-def add_auth_information_to_env(start_env: dict):
-    auth_info = AuthorizationHandler()
-    auth_file_path = str(auth_info.auth_file)
-    # TODO: instead of using strings directly, bring this under an object
-    #       that does validation of UUIDs etc.
-    start_env["auth_file_path"] = auth_file_path
-    start_env["user_id"] = auth_info.user_id
-    start_env["api_token"] = auth_info.api_token
+# https://docs.python.org/3/library/dataclasses.html
+class HoplaEnvironment:
+    def add_auth_information_to_env(self, start_env: dict):
+        auth_info = AuthorizationHandler()
+        auth_file_path = str(auth_info.auth_file)
+        # TODO: instead of using strings directly, bring this under an object
+        #       that does validation of UUIDs etc.
+        start_env["auth_file_path"] = auth_file_path
+        start_env["user_id"] = auth_info.user_id
+        start_env["api_token"] = auth_info.api_token
 
-    log.debug(f"auth_file={auth_file_path}")
+        log.debug(f"auth_file={auth_file_path}")
 
-    return start_env
+        return start_env
 
+    def create_hopla_env(self, script_dirname: str):
+        library_dir = script_dirname + "/library"
+        hopla_env = os.environ
+        hopla_env["script_dirname"] = script_dirname
+        hopla_env["library_dir"] = library_dir
+        hopla_env = self.add_auth_information_to_env(dict(hopla_env))
 
-def create_hopla_env(script_dirname: str):
-    library_dir = script_dirname + "/library"
-    hopla_env = os.environ
-    hopla_env["script_dirname"] = script_dirname
-    hopla_env["library_dir"] = library_dir
-    hopla_env = add_auth_information_to_env(dict(hopla_env))
-
-    return hopla_env
-
-
-def execute_hopla_dot_shell():
-    script_dirname = os.path.dirname(Path(__file__).resolve())
-    cmd_entry = script_dirname + "/hopla.sh"
-    hopla_env = create_hopla_env(script_dirname)
-    cmdline_args = sys.argv[1:]
-    log.debug(f"About to run cmd={cmd_entry} with args: {cmdline_args}")
-    subprocess.run(args=[cmd_entry] + cmdline_args,
-                   env=hopla_env)
+        return hopla_env
 
 
 if __name__ == "__main__":
-    log.debug("start application")
-    execute_hopla_dot_shell()
+    log.debug(f"start application with arguments: {sys.argv}")
+    script_dirname = os.path.dirname(Path(__file__).resolve())
+    # cmd_entry = script_dirname + "/hopla.sh"
+    # cmdline_args = sys.argv[1:]
+    hopla_env = HoplaEnvironment().create_hopla_env(script_dirname=script_dirname)
+
+    arg_parser = ArgumentParser(
+        prog="hopla",
+        description="a CLI to interact with the habitica.com API")
+
+    arg_parser.parse_args()
+
+    # log.debug(f"About to run cmd={cmd_entry} with args: {cmdline_args}")
+    # subprocess.run(args=[cmd_entry] + cmdline_args,
+    #                env=hopla_env)
+
+#
+# class GlobalOptionCmdLineParser:
+#     def __init__(self, *, cmdline_args: List[str]):
+#         self.cmdline_args = cmdline_args
+#         self._help_option_enabled = False
+#
+#         # parse on init
+#         self._parse_global_options()
+#
+#     def __str__(self) -> str:
+#         return GlobalOptionCmdLineParser.__name__
+#
+#     # TODO: upgrade to argparser later on
+#     def _parse_global_options(self):
+#         for argument in self.cmdline_args:
+#             longhelp = "--help"
+#             shorthelp = "-h"
+#             if argument == longhelp:
+#                 self._help_option_enabled = True
+#                 self.cmdline_args.remove(longhelp)
+#             elif argument == shorthelp:
+#                 self._help_option_enabled = True
+#                 self.cmdline_args.remove(shorthelp)
+#
+#     @property
+#     def help_option_enabled(self):
+#         return self._help_option_enabled
+
+
+# class HelpHandler:
+#     # TODO: remove implicit assumption that --help is the only kind
+#     def __init__(self, *, cmdline_args: List[str]):
+#         self.cmdline_args = cmdline_args
+#
+#     def print_help_for_given_cmdline_args(self):
+#         script_dirname: Path = Path(__file__).resolve().parent
+#         cmds_dirname: Path = script_dirname / 'hopla'
+#
+#         # TODO handle implicit assumption that only 2 subcmds exist
+#         if self.first_arg is not None:
+#             possible_subcmd: Path = cmds_dirname / self.first_arg
+#             if (possible_subcmd / ".py").is_file() or (possible_subcmd / ".sh").is_file():
+#                 help_file = possible_subcmd / ".help"
+#                 assert help_file.is_file(), "expected help to exist"
+#                 # found help request
+#                 subprocess.call(["cat", str(help_file)])
+#                 exit(0)
+#
+#     @property
+#     def first_arg(self):
+#         try:
+#             return self.cmdline_args[0]
+#         except IndexError:
+#             return None
+#
+#     @property
+#     def second_arg(self):
+#         try:
+#             return self.cmdline_args[2]
+#         except IndexError:
+#             return None
+#
