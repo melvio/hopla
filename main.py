4e986fe0246fe889d1e0dcb2d3b5cbb71457df58
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-22 18:49:11 +0200

    provide -h alias for --help for all commands

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ class HoplaEnvironment:
         return hopla_env
 
 
+from hopla.subgroups.add import add
 from hopla.subgroups.api import api
 from hopla.subgroups.set import set
 from hopla.subcommands.version import version
@@ -57,7 +58,7 @@ from hopla.subgroups.buy import buy
 from hopla.subgroups.get import get
 
 
-@click.group()
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
 def hopla():
     pass
 
@@ -66,6 +67,7 @@ if __name__ == "__main__":
     log.debug(f"start application with arguments: {sys.argv}")
     script_dirname = os.path.dirname(Path(__file__).resolve())
     hopla_env = HoplaEnvironment().create_hopla_env(script_dirname=script_dirname)
+    hopla.add_command(add)
     hopla.add_command(api)
     hopla.add_command(set)
     hopla.add_command(version)
