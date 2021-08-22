061644cb6a8d9e25feb8d3f933ec23ee4ccaf700
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-22 11:10:35 +0200

    Implement 'hopla user get-stats [stat_name]'
    1. "all" is the default
    2. added class as an option
    3. yet to implement:
       1. aliases: such as mp=mana=manapoints, gp=gold, etc
       2. --filter

--- developers/quick-curl.sh
+++ developers/quick-curl.sh
@@ -6,6 +6,15 @@ this_script_parent_dir=$(realpath "$(dirname "${this_script}")/..")
 source "${this_script_parent_dir}/developer.env"
 
 
+# get all user info
+curl -XGET "https://habitica.com/api/v3/user" \
+  --verbose \
+  --header "Content-Type: application/json" \
+  --header "x-api-user: ${user_id}" \
+  --header "x-api-key: ${api_token}" \
+  --header "x-client: Testing" | jq .
+
+# get user info by userFields
 #curl -XGET "https://habitica.com/api/v3/user?userFields=achievements,items.mounts" \
 #  --verbose \
 #  --header "Content-Type: application/json" \
@@ -17,9 +26,9 @@ source "${this_script_parent_dir}/developer.env"
 #  --verbose \
 #  --header "Content-Type: application/json" | jq .
 
-curl -XGET "https://habitica.com/api/v3/status" \
-  --verbose \
-  --header "Content-Type: application/json" | jq .
+#curl -XGET "https://habitica.com/api/v3/status" \
+#  --verbose \
+#  --header "Content-Type: application/json" | jq .
 
 
 
--- hopla/subgroups/get.py
+++ hopla/subgroups/get.py
@@ -18,12 +18,9 @@ valid_item_groups = click.Choice(["pets", "mounts", "food", "gear", "quests", "h
 @get.command(help="Get items from the user's inventory")
 @click.argument("item_group_name", type=valid_item_groups, default="all")
 def user_inventory(item_group_name):
-    log.debug("hopla get user-inventory")
+    log.debug(f"hopla get user-inventory item_group={item_group_name}")
 
-    headers = RequestHeaders().get_default_request_headers()
-    url = UrlBuilder(path_extension="/user").url
-
-    response = requests.get(url=url, headers=headers)
+    response = HabiticaUserRequest().get_user()
 
     json = response.json()
     if json["success"]:
@@ -34,3 +31,36 @@ def user_inventory(item_group_name):
             click.echo(data_items[item_group_name])
     else:
         click.echo(json["message"])
+
+
+valid_stat_names = click.Choice(["hp", "mp", "exp", "gp", "class", "all"])
+
+
+@get.command(help="Get a user's stats")
+@click.argument("stat_name", type=valid_stat_names, default="all")
+def user_stats(stat_name):
+    """
+    Retrieve information from /user .data.user.stats
+    """
+    log.debug(f"hopla get user-stats stat={stat_name}")
+
+    response = HabiticaUserRequest().get_user()
+
+    json = response.json()
+    if json["success"]:
+        data_items = json["data"]["stats"]
+        if stat_name == "all":
+            click.echo(data_items)
+        else:
+            click.echo(data_items[stat_name])
+    else:
+        click.echo(json["message"])
+
+
+class HabiticaUserRequest:
+    def __init__(self):
+        self.url = UrlBuilder(path_extension="/user").url
+        self.headers = RequestHeaders().get_default_request_headers()
+
+    def get_user(self) -> requests.Response:
+        return requests.get(url=self.url, headers=self.headers)
