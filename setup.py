da3baa9907d629ab3a3199b63fbfbb038dbeca01
Melvin <31448155+melvio@users.noreply.github.com>
2021-08-27 21:03:19 +0200

    introduce semantic versioning

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 from setuptools import setup
 
-# TODO: get this value injected into `hopla version`
-HOPLA_VERSION = "0.0.1"
+MAJOR_VERSION = 0
+MINOR_VERSION = 0
+PATCH_VERSION = 1
+PRE_RELEASE = "alpha"
+
+
+def hopla_version() -> str:
+    # <https://semver.org/>
+    # TODO: get this value injected into `hopla version`
+
+    version_core = f"{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}"
+    if PRE_RELEASE is not None and PRE_RELEASE is not "":
+        pre_release = f"-{PRE_RELEASE}"
+    else:
+        pre_release = ""
+
+    return version_core + pre_release
+
 
 setup(
     name="hopla",
-    version=HOPLA_VERSION,
+    version=hopla_version(),
+    author="Melvin",
+    url="https://github.com/melvio/hopla",
     py_modules=["main"],
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
     install_requires=[
