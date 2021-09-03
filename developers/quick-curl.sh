fd7e67dcc47cf80b5e94c3cb6056a56fa49cd89f
Melvin <31448155+melvio@users.noreply.github.com>
2021-09-03 21:42:59 +0200

    query the group/party

--- developers/quick-curl.sh
+++ developers/quick-curl.sh
@@ -6,12 +6,12 @@ this_script_parent_dir=$(realpath "$(dirname "${this_script}")")
 source "${this_script_parent_dir}/developer.env"
 
 # get all user info
-curl -XGET "https://habitica.com/api/v3/user" \
-  --verbose \
-  --header "Content-Type: application/json" \
-  --header "x-api-user: ${user_id}" \
-  --header "x-api-key: ${api_token}" \
-  --header "x-client: Testing" | jq .
+#curl -XGET "https://habitica.com/api/v3/user" \
+#  --verbose \
+#  --header "Content-Type: application/json" \
+#  --header "x-api-user: ${user_id}" \
+#  --header "x-api-key: ${api_token}" \
+#  --header "x-client: Testing" | jq .
 
 # get user info by userFields
 #curl -XGET "https://habitica.com/api/v3/user?userFields=achievements.streak,purchased.plan" \
@@ -29,7 +29,12 @@ curl -XGET "https://habitica.com/api/v3/user" \
 #  --verbose \
 #  --header "Content-Type: application/json" | jq .
 
-
+curl -XGET "https://habitica.com/api/v3/groups/party" \
+  --verbose \
+  --header "Content-Type: application/json" \
+  --header "x-api-user: ${user_id}" \
+  --header "x-api-key: ${api_token}" \
+  --header "x-client: Testing" | jq .
 
 # [docs](https://habitica.com/apidoc/#api-User-UserFeed)
 # [API PATH](https://habitica.com/api/v3/user/feed/:pet/:food)
