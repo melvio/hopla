5c06ac09306d6d39941e0ee8ed7e6d3a5207bc89
Melvin <31448155+melvio@users.noreply.github.com>
2021-09-05 20:29:28 +0200

    Improve test quality

--- src/tests/cli/groupcmds/test_buy.py
+++ src/tests/cli/groupcmds/test_buy.py
@@ -1,13 +1,12 @@
 from hopla.cli.groupcmds.buy import times_until_poor
 
 
-def test_times_until_poor__budget_is0():
-    assert times_until_poor(0.) == 0
+class TestTimesUntilPoor:
+    def test_times_until_poor__budget_is0(self):
+        assert times_until_poor(0.) == 0
 
+    def test_times_until_poor__budget_is_below100(self):
+        assert times_until_poor(92.3) == 0
 
-def test_times_until_poor__budget_is_below100():
-    assert times_until_poor(92.3) == 0
-
-
-def test_times_until_poor__budget_is_over100():
-    assert times_until_poor(212) == 2
+    def test_times_until_poor__budget_is_over100(self):
+        assert times_until_poor(212) == 2
