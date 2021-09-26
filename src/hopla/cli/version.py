cede15733748fdae1394645a1ca95105c0cbcb35
Melvin <31448155+melvio@users.noreply.github.com>
2021-09-26 21:35:05 +0200

    Refactored version logic

--- src/hopla/cli/version.py
+++ src/hopla/cli/version.py
@@ -6,29 +6,9 @@ import logging
 
 import click
 
+from hopla.hoplalib.hoplaversion import HoplaVersion
+
 log = logging.getLogger()
-
-MAJOR_VERSION = 0
-MINOR_VERSION = 0
-PATCH_VERSION = 16
-PRE_RELEASE = "alpha"
-
-
-def hopla_version() -> str:
-    """
-    Prints the hopla version (Not to be confused with the habitica API version)
-
-    The output should satisfy [semantic versioning](https://semver.org/)
-    TODO: verify if this is the case
-    """
-
-    version_core = f"{MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}"
-    if PRE_RELEASE is not None and PRE_RELEASE != "":
-        pre_release = f"-{PRE_RELEASE}"
-    else:
-        pre_release = ""
-
-    return version_core + pre_release
 
 
 @click.command()
@@ -39,4 +19,4 @@ def version():
     :return:
     """
     log.debug("hopla version")
-    click.echo(hopla_version())
+    click.echo(HoplaVersion().full_version())
