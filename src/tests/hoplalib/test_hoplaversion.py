d68ca1fb791c1164941333fd5b4e039afff2b780
Melvin <31448155+melvio@users.noreply.github.com>
2021-09-27 18:14:52 +0200

    Review updates

--- src/tests/hoplalib/test_hoplaversion.py
+++ src/tests/hoplalib/test_hoplaversion.py
@@ -10,25 +10,25 @@ from hopla.hoplalib.hoplaversion import HoplaVersion
 
 class TestHoplaVersion:
 
-    def test_full_version_defaults(self, semver_pattern: Pattern):
+    def test_semantic_version_defaults(self, semver_pattern: Pattern):
         hopla_version = HoplaVersion()
-        result: str = hopla_version.full_version()
+        result: str = hopla_version.semantic_version()
         assert re.fullmatch(pattern=semver_pattern, string=result)
 
     @pytest.mark.parametrize("prerelease_value", [None, ""])
-    def test_full_version_without_prerelease(self, semver_pattern: Pattern,
-                                             prerelease_value: Optional[str]):
+    def test_semantic_version_without_prerelease(self, semver_pattern: Pattern,
+                                                 prerelease_value: Optional[str]):
         hopla_version = HoplaVersion(pre_release=prerelease_value)
-        result: str = hopla_version.full_version()
+        result: str = hopla_version.semantic_version()
         assert re.fullmatch(pattern=semver_pattern, string=result)
 
-    def test_full_version_ok(self, semver_pattern: Pattern):
+    def test_semantic_version_ok(self, semver_pattern: Pattern):
         hopla_version = HoplaVersion(major_version=123,
                                      minor_version=200,
                                      patch_version=69,
                                      pre_release="beta")
 
-        result: str = hopla_version.full_version()
+        result: str = hopla_version.semantic_version()
         assert result == "123.200.69-beta"
         assert re.fullmatch(pattern=semver_pattern, string=result)
 
