d3928abd53e1abf8b0b009a4e98e2d4b7820077f
Melvin <31448155+melvio@users.noreply.github.com>
2021-10-12 12:40:20 +0200

    release v0.0.26

--- src/hopla/hoplalib/hoplaversion.py
+++ src/hopla/hoplalib/hoplaversion.py
@@ -7,7 +7,7 @@ from typing import Final, Optional
 
 MAJOR_VERSION: Final[int] = 0
 MINOR_VERSION: Final[int] = 0
-PATCH_VERSION: Final[int] = 25
+PATCH_VERSION: Final[int] = 26
 PRE_RELEASE: Final[Optional[str]] = "alpha"
 
 
