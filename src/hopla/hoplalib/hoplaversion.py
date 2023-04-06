b4862d3d6a9fb57dbc07af0be032dd0cdf4358bd
Melvin <31448155+melvio@users.noreply.github.com>
2023-04-06 21:20:23 +0200

    update to patch version 33

--- src/hopla/hoplalib/hoplaversion.py
+++ src/hopla/hoplalib/hoplaversion.py
@@ -7,7 +7,7 @@ from typing import Final, Optional
 
 MAJOR_VERSION: Final[int] = 0
 MINOR_VERSION: Final[int] = 0
-PATCH_VERSION: Final[int] = 32
+PATCH_VERSION: Final[int] = 33
 PRE_RELEASE: Final[Optional[str]] = "alpha"
 
 
