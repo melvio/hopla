ae7e0b54ec7eed652bdf44adbf4e157ab42bb00f
Melvin <31448155+melvio@users.noreply.github.com>
2024-04-21 09:05:30 +0200

    release 0.0.37 with funghi

--- src/hopla/hoplalib/hoplaversion.py
+++ src/hopla/hoplalib/hoplaversion.py
@@ -7,7 +7,7 @@ from typing import Final, Optional
 
 MAJOR_VERSION: Final[int] = 0
 MINOR_VERSION: Final[int] = 0
-PATCH_VERSION: Final[int] = 36
+PATCH_VERSION: Final[int] = 37
 PRE_RELEASE: Final[Optional[str]] = "alpha"
 
 
